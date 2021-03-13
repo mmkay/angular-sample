4b6ffb1be6184e94afea988767ed9fe6a64d9406
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2021-03-13 20:13:31 +0100

    ngStyle usage example

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -6,9 +6,17 @@ import { Component } from '@angular/core';
 })
 export class ServerComponent {
   serverId = 10;
-  status = 'OFFLINE';
+  status = 'offline';
+
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getServerStatus() {
     return this.status;
+  }
+
+  getColor() {
+    return this.status === 'online' ? 'green' : 'red';
   }
 }

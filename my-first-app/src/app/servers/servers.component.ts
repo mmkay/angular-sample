f9ad8ca55a822e6d35741963756ab767797944d6
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2021-03-13 20:04:41 +0100

    *ngIf directive use example

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => { this.allowNewServer = true; }, 2000);
@@ -18,6 +19,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
